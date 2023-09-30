9704cb0e47d66d089cdca8968a5ea9eca15d4af7
pavanm100 <email>
2023-09-30 17:47:32 -0400

    added contact code

--- src/Contact.java
+++ src/Contact.java
@@ -2,9 +2,29 @@
  This is the one sentence, descriptive summary, part of a doc comment.
  There can be more lines after the first one if necessary.
  ....
- @author Araika Khokhar, Pavan Machi
+ @author Araika Khokhar, Pavan Machhi
  ...
  */
 
 public class Contact {
+ private Department department;
+ private String email;
+
+ public enum Department {
+  CS, EE, ITI, MATH, BAIT;
+ }
+
+ // Constructor
+ public Contact(Department department, String email) {
+  this.department = department;
+  this.email = email;
+ }
+
+ // isValid method with correct curly braces
+ public boolean isValid() {
+  boolean validDepartment = department != null;
+  boolean validEmail = email != null && email.endsWith("@rutgers.edu");
+  return validDepartment && validEmail;
+ }
+
 }
